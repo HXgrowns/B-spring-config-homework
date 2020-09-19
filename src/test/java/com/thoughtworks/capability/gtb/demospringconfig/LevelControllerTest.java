0828f89677a5a02e69e03c173405c83ffb470df2
HXgrowns <804156017@qq.com>
2020-09-20 01:06:47 +0800

    [huxiao]:p2s2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -13,9 +14,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
+@TestPropertySource(locations = "/lication-test.properties")
 class LevelControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -23,7 +26,7 @@ class LevelControllerTest {
     @Test
     void getLevel() throws Exception {
         mockMvc.perform(get("http://localhost:8080/level"))
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.is("advanced")));
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.is("advanced")));
     }
 }
