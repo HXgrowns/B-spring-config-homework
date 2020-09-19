0adeff9fdcfe95af25fb784beeeb4d9269b3a31f
HXgrowns <804156017@qq.com>
2020-09-20 01:04:37 +0800

    [huxiao]:p2s1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class LevelControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getLevel() throws Exception {
+        mockMvc.perform(get("http://localhost:8080/level"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.is("advanced")));
+    }
+}
